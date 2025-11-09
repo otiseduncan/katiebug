37f63358c9e1ae70267005c51eb1c282083e0b39
Otis Duncan <otiseduncan@users.noreply.github.com>
2025-11-09 08:40:15 -0500

    Remove deprecated next export command for Netlify build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true
-  },
-  output: "export"
+  output: 'export',          // ✅ tells Next.js to statically export
+  distDir: 'out',            // ✅ makes the output folder match Netlify’s publish dir
+  reactStrictMode: true,
 };
+
 module.exports = nextConfig;
